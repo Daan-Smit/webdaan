3f25b56656955d310a6e51dc59248c53197137ba
Daan-Smit <daan.smit@kpnmail.nl>
2023-03-08 17:17:32 +0800

    update

--- index.php
+++ index.php
@@ -2,20 +2,7 @@
     include 'main/head.php';
     include 'main/header.php';
 ?>
-    <div class="containter">
-        <div class="row">
-            <div class="col-sm">
-                <h1>hello there</h1>
-            </div>
-            <div class="col-sm">
-                One of three columns
-            </div>
-            <div class="col-sm">
-                One of three columns
-            </div>
-        </div>
-    </div>
-    <img src="images/logos/color-no-bg.png" alt="">
+    
 <?php
     include 'main/footer.php';
 ?>
