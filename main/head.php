9394479f080324afc96c1884032a9e7796402a1d
Daan-Smit <daan.smit@kpnmail.nl>
2023-03-09 13:03:08 +0800

    Update head.php

--- main/head.php
+++ main/head.php
@@ -26,7 +26,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <!-- CSS Files -->
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="./css/style.css">
     <?php //echo $path . "/css/style.css"?>
 </head>
 <body>
